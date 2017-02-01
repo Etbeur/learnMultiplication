c66a3d85787e97e2931f7bbc134ae365ebdad183
etbeur <romain.etheve@gmail.com>
2017-02-01 23:55:18 +0100

    modif index et nom table dans connexion

--- php/index.php
+++ php/index.php
@@ -10,43 +10,41 @@ session_start();
     <title>Les tables de multiplications</title>
     <link rel="stylesheet" href="/css/style.min.css"/>
   </head>
-f
-<body>
-    <?php  ?>
-    <div id="tete">
+	<body>
+		<div id="tete">
 
-        <div class="infos_user">
-            <?php
-                // Si aucune session n'est active (utilisateur non connecté), on le précise
-                if(!isset($_SESSION['user'])){
-                    echo '<p>Vous êtes actuellement sur le site en tant que visiteur.</p>';
-                // Si une session est existante, on crée un boutton de déconnexion et on indique au visiteur qu'il est connecté
-                }else{
-                    echo '<p>Connecté en tant que '. $_SESSION['user'] . '!</p>';
-                    echo '<a href="end_session.php?logout">Deconnexion</a>';
-                }
-             ?>
-         </div>
+		    <div class="infos_user">
+		        <?php
+		            // Si aucune session n'est active (utilisateur non connecté), on le précise
+		            if(!isset($_SESSION['user'])){
+		                echo '<p>Vous êtes actuellement sur le site en tant que visiteur.</p>';
+		            // Si une session est existante, on crée un boutton de déconnexion et on indique au visiteur qu'il est connecté
+		            }else{
+		                echo '<p>Connecté en tant que '. $_SESSION['user'] . '!</p>';
+		                echo '<a href="end_session.php?logout">Deconnexion</a>';
+		            }
+		         ?>
+		     </div>
 
-        <h1>Learn multiplication</h1>
-        <p>Bienvenue sur ce site qui permet d'apprendre les tables de multiplications facilement tout en s'amusant.</p>
-        <p>Pensez à vous connecter ou à créer un compte en allant dans la partie mon compte.</p>
-    </div>
+		    <h1>Learn multiplication</h1>
+		    <p>Bienvenue sur ce site qui permet d'apprendre les tables de multiplications facilement tout en s'amusant.</p>
+		    <p>Pensez à vous connecter ou à créer un compte en allant dans la partie mon compte.</p>
+		</div>
 
-    <!-- Barre de navigation du menu principale, organisé en colonne -->
+		<!-- Barre de navigation du menu principale, organisé en colonne -->
 
-    <nav>
-        <a class="lien_nav" href="table.php" id="table">Accès aux tables</a>
-        <a class="lien_nav" href="exercices_en_cours_de_realisation.php" id="exercice">Accès aux exercices</a>
-        <a class="lien_nav" href= "connexion_compte.php" id="compte">Mon compte</a>
-    </nav>
+		<nav>
+		    <a class="lien_nav" href="table.php" id="table">Accès aux tables</a>
+		    <a class="lien_nav" href="exercices_en_cours_de_realisation.php" id="exercice">Accès aux exercices</a>
+		    <a class="lien_nav" href= "connexion_compte.php" id="compte">Mon compte</a>
+		</nav>
 
-    <footer id="pied_de_page">
-        <p>
-            Designed and created by <a href="https://github.com/etbeur">Etbeur</a> - 2016
-        </p>
-    </footer>
+		<footer id="pied_de_page">
+		    <p>
+		        Designed and created by <a href="https://github.com/etbeur">Etbeur</a> - 2016
+		    </p>
+		</footer>
 
-<script type="text/javascript" src="/JS/script.js"></script>
-</body>
+	<script type="text/javascript" src="/JS/script.js"></script>
+	</body>
 </html>
